dab8fa940b8880357516162ffd3bd6a898d3e6f0
Tshego-eng <tshegofatsoramokopu307@gmail.com>
2026-06-18 15:21:28 +0200

    API key

--- Site/functions/test.js
+++ Site/functions/test.js
@@ -1,3 +1,5 @@
-export async function onRequest() {
-  return new Response("Pages Functions are working!");
+export async function onRequest(context) {
+  return new Response(
+    context.env.RESEND_API_KEY ? "Key Found" : "Key Missing"
+  );
 }
